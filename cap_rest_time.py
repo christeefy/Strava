6fc72cd809659dc02175f4b7ef11e2fd5f5b1dd7
christeefy <fuyuan.tee@mail.utoronto.ca>
2018-08-07 21:52:03 -0400

    Change algorithm to minimise number of timestamp modifications.

--- cap_rest_time.py
+++ cap_rest_time.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='Script that limit rests in GPX fil
 # Add arguments
 parser.add_argument('src_dir', help='directory of the source file')
 parser.add_argument('-sc', '--save_copy', action='store_true', help='Save output file as a copy with `copy` appended to filename')
-parser.add_argument('-mrt', '--MAX_REST_TIME', type=int, default=45, help='All rest time will be limited to this value (in minutes).')
+parser.add_argument('-mrt', '--MAX_REST_TIME', type=int, default=45, help='All rest times will be capped at this value (in minutes).')
 
 # Parse arguments
 args = parser.parse_args()
@@ -29,27 +29,14 @@ for k, v in namespaces.items():
 # Find all <time> tags
 times = tree.findall('.//ns:time', namespaces={'ns': namespaces['']})
 
-# Define variable that tracks critical time indices where time difference exceeds the MAX_TIME_LIMIT
-crit_time_indices = []
-
 # Initial loop to find and tag number of tags exceeding MAX_TIME_LIMIT
 for i in range(len(times) - 1):
     curr_time = datetime.datetime.strptime(times[i].text, '%Y-%m-%dT%H:%M:%SZ')
     next_time = datetime.datetime.strptime(times[i + 1].text, '%Y-%m-%dT%H:%M:%SZ')
     
     if (next_time - curr_time).seconds // 60 > args.MAX_REST_TIME:
-        crit_time_indices.append(i + 1)
-        
-    
-# Modify timestamp based on crit_time_indices
-for i in range(len(times)):
-    num_shifts = sum([idx <= i for idx in crit_time_indices])
-    
-    if num_shifts:
-        mod_time = datetime.datetime.strptime(times[i].text, '%Y-%m-%dT%H:%M:%SZ') \
-                   - datetime.timedelta(minutes=num_shifts * args.MAX_REST_TIME)
-        
-        times[i].text = mod_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+        next_time -= datetime.timedelta(minutes=args.MAX_REST_TIME)
+        times[i + 1].text = next_time.strftime('%Y-%m-%dT%H:%M:%SZ')
 
 # Save file
 if args.save_copy:
